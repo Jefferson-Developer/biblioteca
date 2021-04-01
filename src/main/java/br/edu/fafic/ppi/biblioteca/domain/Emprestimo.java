90b8288a5249ef4ccb523660846ece536323d26c
Erlon Jr <ejrgeek@gmail.com>
2021-03-31 23:31:21 -0300

    chore: adiciona campo de data de emprestimo no model

--- src/main/java/br/edu/fafic/ppi/biblioteca/domain/Emprestimo.java
+++ src/main/java/br/edu/fafic/ppi/biblioteca/domain/Emprestimo.java
@@ -1,12 +1,10 @@
 package br.edu.fafic.ppi.biblioteca.domain;
 
-import br.edu.fafic.ppi.biblioteca.dto.LivroDTO;
-import br.edu.fafic.ppi.biblioteca.enums.Area;
 import lombok.*;
-import org.modelmapper.ModelMapper;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,6 +23,8 @@ public class Emprestimo implements Serializable {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Usuario usuario;
+
+    private Calendar dataEmprestimo;
 
     @ManyToMany(cascade = CascadeType.ALL)
     private List<Livro> livrosLocados;
